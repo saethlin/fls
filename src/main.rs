d58934ca154a055b36dbc139daeff8600c7d3a53
Ben Kimock <kimockb@gmail.com>
2019-06-23 02:19:54 -0400

    do coloring without calling lstat

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,144 @@
-use rayon::prelude::*;
-use std::fs;
 use std::path::Path;
 
-use std::path::PathBuf;
-use structopt::StructOpt;
-
-#[derive(Debug, StructOpt)]
-#[structopt(name = "fls", about = "Experiments in a fast ls implementation")]
-struct Opt {
-    #[structopt(parse(from_os_str))]
-    files: Vec<PathBuf>,
-    #[structopt(short = "R", long = "recursive")]
-    recurse: bool,
+struct ReadDir<'a> {
+    dir: *mut libc::DIR,
+    marker: std::marker::PhantomData<&'a libc::DIR>,
 }
 
-fn get_entries<P: AsRef<Path>>(dir: P, output: &mut Vec<DirEntry>) -> Result<(), std::io::Error> {
-    let entries: Result<Vec<_>, _> = fs::read_dir(dir)?.map(|r| r.map(|e| e.path())).collect();
-    let entries = entries?;
-
-    output.reserve(entries.len());
-
-    entries
-        .into_par_iter()
-        .map(|path| {
-            let mut color = Color::White;
-            let mut style = Style::Regular;
-            if let Ok(metadata) = fs::symlink_metadata(&path) {
-                if metadata.file_type().is_symlink() {
-                    if fs::metadata(&path).is_err() {
-                        color = Color::Red;
-                    } else {
-                        color = Color::Cyan;
-                    }
-                } else if metadata.file_type().is_dir() {
-                    color = Color::Blue;
-                    style = Style::Bold;
-                }
+impl<'a> ReadDir<'a> {
+    fn new<P: AsRef<Path>>(path: P) -> Option<Self> {
+        use std::ffi::CString;
+        use std::os::unix::ffi::OsStrExt;
+        let path = CString::new(path.as_ref().as_os_str().as_bytes()).ok()?;
+        unsafe {
+            let dir = libc::opendir(path.as_ptr());
+            if dir.is_null() {
+                None
+            } else {
+                Some(Self {
+                    dir,
+                    marker: Default::default(),
+                })
             }
-            DirEntry { style, color, path }
-        })
-        .collect_into_vec(output);
-
-    Ok(())
+        }
+    }
 }
 
-fn print_contents_of<P: AsRef<Path>, W: std::io::Write>(
-    dir: P,
-    w: &mut W,
-    entries: &mut Vec<DirEntry>,
-    recurse: bool,
-) -> Result<(), std::io::Error> {
-    get_entries(dir, entries)?;
+#[derive(Debug)]
+enum DType {
+    Fifo = 1,
+    Character = 2,
+    Directory = 4,
+    Block = 6,
+    Regular = 8,
+    Symlink = 10,
+    Socket = 12,
+}
 
-    let mut previous_style = Style::Regular;
-    let mut previous_color = Color::White;
-    for DirEntry { style, color, path } in entries.iter() {
-        let path: &Path = path.file_name().unwrap().as_ref();
-        if *style != previous_style {
-            write!(w, "{}", *style)?;
-            previous_style = *style;
+#[derive(Debug)]
+struct RawDirEntry<'a> {
+    name: &'a CStr,
+    d_type: Option<DType>,
+}
+
+use std::ffi::CStr;
+impl<'a> Iterator for ReadDir<'a> {
+    type Item = RawDirEntry<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            let ptr = libc::readdir(self.dir);
+            if ptr.is_null() {
+                None
+            } else {
+                let name = CStr::from_ptr(&(*ptr).d_name as *const i8);
+                let d_type = match (*ptr).d_type {
+                    libc::DT_UNKNOWN => None,
+                    libc::DT_FIFO => Some(DType::Fifo),
+                    libc::DT_CHR => Some(DType::Character),
+                    libc::DT_DIR => Some(DType::Directory),
+                    libc::DT_BLK => Some(DType::Block),
+                    libc::DT_REG => Some(DType::Regular),
+                    libc::DT_LNK => Some(DType::Symlink),
+                    libc::DT_SOCK => Some(DType::Socket),
+                    _ => None,
+                };
+                Some(RawDirEntry { name, d_type })
+            }
         }
-        if *color != previous_color {
-            write!(w, "{}", *color)?;
-            previous_color = *color;
-        }
-        writeln!(w, "{}", path.display())?;
     }
-    write!(
-        w,
-        "{}{}",
-        termion::color::Fg(termion::color::Reset),
-        termion::style::Reset
-    )?;
+}
 
-    if recurse {
-        let dirs = entries
-            .drain(..)
-            .filter_map(|e| {
-                if e.color == Color::Blue {
-                    Some(e.path)
-                } else {
-                    None
+impl<'a> RawDirEntry<'a> {
+    fn style(&self, root: &mut Vec<u8>) -> (Color, Style) {
+        use Color::*;
+        use Style::*;
+        match self.d_type.as_ref().unwrap() {
+            DType::Directory => (Blue, Bold),
+            DType::Symlink => (Cyan, Bold),
+            DType::Regular => unsafe {
+                if root.last() != Some(&b'/') {
+                    root.push(b'/');
                 }
-            })
-            .collect::<Vec<_>>();
-        for d in dirs {
-            writeln!(w, "\n{}", d.display())?;
-            print_contents_of(d, w, entries, true)?;
+                root.extend(self.name.to_bytes());
+                root.push(0);
+                let executable = libc::access(root.as_ptr() as *const i8, libc::X_OK);
+                while root.last() != Some(&b'/') {
+                    root.pop();
+                }
+                if executable == 0 {
+                    (Green, Bold)
+                } else {
+                    (White, Regular)
+                }
+            },
+            _ => (White, Regular),
         }
     }
-
-    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     use std::io::Write;
-    let opt = Opt::from_args();
-
     let stdout = std::io::stdout();
+    //let mut buf = stdout.lock();
     let mut buf = std::io::BufWriter::new(stdout.lock());
+    write!(buf, "{}{}", Color::White, Style::Regular)?;
 
-    let mut entries = Vec::new();
-    if opt.files.is_empty() {
-        print_contents_of(".", &mut buf, &mut entries, opt.recurse)?;
-    } else if opt.files.len() == 1 {
-        print_contents_of(&opt.files[0], &mut buf, &mut entries, opt.recurse)?;
+    let mut previous_color = Color::White;
+    let mut previous_style = Style::Regular;
+    let args = std::env::args();
+
+    if args.len() < 2 {
+        let mut root = vec![b'.'];
+        for e in ReadDir::new(".").unwrap() {
+            let (color, style) = e.style(&mut root);
+            if color != previous_color {
+                write!(buf, "{}", color)?;
+                previous_color = color;
+            }
+            if style != previous_style {
+                write!(buf, "{}", style)?;
+                previous_style = style;
+            }
+            buf.write_all(e.name.to_bytes())?;
+            buf.write_all(b"\n")?;
+        }
     } else {
-        for target in &opt.files {
-            writeln!(&mut buf, "{}", target.display())?;
-            print_contents_of(target, &mut buf, &mut entries, opt.recurse)?;
+        for arg in args.skip(1) {
+            let mut root = arg.as_bytes().iter().cloned().collect();
+            for e in ReadDir::new(arg).unwrap() {
+                let (color, style) = e.style(&mut root);
+                if color != previous_color {
+                    write!(buf, "{}", color)?;
+                    previous_color = color;
+                }
+                if style != previous_style {
+                    write!(buf, "{}", style)?;
+                    previous_style = style;
+                }
+                buf.write_all(e.name.to_bytes())?;
+                buf.write_all(b"\n")?;
+            }
         }
     }
 
@@ -122,28 +152,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-struct DirEntry {
-    path: PathBuf,
-    color: Color,
-    style: Style,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum Color {
     White,
-    Red,
     Cyan,
     Blue,
+    Green,
 }
 
 impl std::fmt::Display for Color {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        use termion::color::{Blue, Cyan, Fg, Red, White};
+        use termion::color::{Blue, Cyan, Fg, Green, White};
         match self {
             Color::White => write!(f, "{}", Fg(White)),
-            Color::Red => write!(f, "{}", Fg(Red)),
             Color::Cyan => write!(f, "{}", Fg(Cyan)),
             Color::Blue => write!(f, "{}", Fg(Blue)),
+            Color::Green => write!(f, "{}", Fg(Green)),
         }
     }
 }
@@ -156,9 +180,9 @@ enum Style {
 
 impl std::fmt::Display for Style {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        use termion::style::{Bold, Reset};
+        use termion::style::{Bold, NoBold};
         match self {
-            Style::Regular => write!(f, "{}", Reset),
+            Style::Regular => write!(f, "{}", NoBold),
             Style::Bold => write!(f, "{}", Bold),
         }
     }
