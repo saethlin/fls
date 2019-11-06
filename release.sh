2a6b70241b4f472da80e456fdba0b075128a5cef
Ben Kimock <kimockb@gmail.com>
2019-11-05 21:27:40 -0500

    update CI/CD

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+git clone --depth=1 https://github.com/saethlin/fls
+cd fls
+cargo build --release
+
+COMMIT_HASH=$(git log -n 1 --pretty=format:"%H" | cut -c-7)
+curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" --data "{\"tag_name\": \"${COMMIT_HASH}\"}" https://api.github.com/repos/saethlin/fls/releases >  /tmp/response
+
+ID=$(python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["id"])' < /tmp/response)
+
+curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" -H "Content-type: application/octet-stream" --data @target/release/fls "https://uploads.github.com/repos/saethlin/loadtxt/releases/${ID}/assets?name=fls"
