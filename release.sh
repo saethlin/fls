21d20050c6eb4df8b123d2ea259b3220ef039f32
Ben Kimock <kimockb@gmail.com>
2020-10-08 22:14:48 -0400

    rework release system

--- release.sh
+++ release.sh
@@ -2,19 +2,23 @@
 
 set -e
 
-git clone --depth=1 https://github.com/saethlin/fls
-cd fls
+curl --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile=minimal --default-toolchain=nightly
+source $HOME/.cargo/env
 
-rustup default nightly
-rustup show
+git clone https://github.com/saethlin/fls --depth=1
+cd fls
 
 cargo build --release
 
-./target/release/fls
+ID=$(curl https://api.github.com/repos/saethlin/fls/releases \
+    --header "Authorization: token ${GITHUB_TOKEN}" \
+    --header "Accept: application/vnd.github.v3+json" \
+    --data "{\"tag_name\": \"$(date -u -Iseconds | cut -d+ -f1 | sed -e s/:/-/g)\"}" | \
+    python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj[\"id\"])"
+)
 
-COMMIT_HASH=$(git log -n 1 --pretty=format:"%H" | cut -c-7)
-curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" --data "{\"tag_name\": \"${COMMIT_HASH}\"}" https://api.github.com/repos/saethlin/fls/releases >  /tmp/response
-
-ID=$(python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["id"])' < /tmp/response)
-
-curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" -H "Content-type: application/octet-stream" --data-binary @target/release/fls "https://uploads.github.com/repos/saethlin/fls/releases/${ID}/assets?name=fls"
+curl "https://uploads.github.com/repos/saethlin/fls/releases/${ID}/assets?name=fls" \
+    --header "Authorization: token ${GITHUB_TOKEN}" \
+    --header "Accept: application/vnd.github.v3+json" \
+    --header "Content-type: application/octet-stream" \
+    --data-binary @target/release/fls
